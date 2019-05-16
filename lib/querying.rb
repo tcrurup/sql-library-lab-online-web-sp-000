2625d2f44c7691007a89c5b61a21aff11dd28ab3
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 17:56:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,6 @@ def select_series_title_with_most_human_characters
   FROM characters
   INNER JOIN series
     ON characters.series_id = series.id
-  WHERE characters.species = 'human'
   GROUP BY series.title
   "
 end
