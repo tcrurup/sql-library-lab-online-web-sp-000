db8c7b31e4fd2e9f03808ca748509c975dca7af7
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 16:53:26 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES("Red Rising", ,2)
-INSERT INTO series (title, author_id, subgenre_id) VALUES("Heart Strikers", ,1)
+INSERT INTO series (title, author_id, subgenre_id) VALUES("Red Rising", 1, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES("Heart Strikers", 2, 1);
   
-INSERT INTO subgenres(name) VALUES("Fantasy")
-INSERT INTO subgenres(name) VALUES("SciFi")
+INSERT INTO subgenres(name) VALUES("Fantasy");
+INSERT INTO subgenres(name) VALUES("SciFi");
 
-INSERT INTO authors(name) VALUES("Pierce Brown")
-INSERT INTO authors(name) VALUES("Rachel Aaron")
+INSERT INTO authors(name) VALUES("Pierce Brown");
+INSERT INTO authors(name) VALUES("Rachel Aaron");
