44bdfb89a5570ffc2dd0dde2a3997ed3eeab3734
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 16:52:18 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,7 @@
-INSERT INTO series (title, author_id, subgenre_id)
-  VALUES("Red Rising")
-INSERT INTO series (title, author_id, subgenre_id)
-  VALUES("Heart Strikers")
+INSERT INTO series (title, author_id, subgenre_id) VALUES("Red Rising", ,2)
+INSERT INTO series (title, author_id, subgenre_id) VALUES("Heart Strikers", ,1)
   
 INSERT INTO subgenres(name) VALUES("Fantasy")
+INSERT INTO subgenres(name) VALUES("SciFi")
+
+INSERT INTO authors
