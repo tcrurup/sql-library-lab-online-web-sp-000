a6d647830d04ae8b000a5f34c7d69e7bcbfb6aa8
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 17:26:18 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,7 +2,8 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT book.title
   FROM books 
   INNER JOIN series
-  ON books.series_id = series.id"
+  ON books.series_id = series.id
+  WHERE series.id = 1"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
