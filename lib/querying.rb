013682016cb1323d07ec1c408412097fa288a943
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 17:40:52 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
-  INNER JOIN subgenres"
+  INNER JOIN subgenres
+  ON "
 end
 
 def select_series_title_with_most_human_characters
