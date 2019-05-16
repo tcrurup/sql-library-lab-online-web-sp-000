bbd0d3db8a9022764fdfced94b7fe983e8cbc449
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 17:07:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -35,3 +35,6 @@ INSERT INTO characters(name, motto, species, author_id, series_id)
 INSERT INTO character_books(character_id, book_id) VALUES(1, 1);
 INSERT INTO character_books(character_id, book_id) VALUES(1, 2);
 INSERT INTO character_books(character_id, book_id) VALUES(1, 3);
+INSERT INTO character_books(character_id, book_id) VALUES(2, 1);
+INSERT INTO character_books(character_id, book_id) VALUES(2, 2);
+INSERT INTO character_books(character_id, book_id) VALUES(2, 3);
