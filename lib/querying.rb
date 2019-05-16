0d6e4252e22fae9ae8ee691e4bfcf433dbdf0da5
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 17:32:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,9 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT 
+  FROM authors
+  INNER JOIN subgenres"
 end
 
 def select_series_title_with_most_human_characters
