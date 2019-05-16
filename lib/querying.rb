71013f4e264d7e526397a1324daacc750c35a828
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 17:58:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,10 +38,10 @@ def select_series_title_with_most_human_characters
     ON characters.series_id = series.id
   WHERE characters.species = 'human'
   GROUP BY series.title
-  LIMIT 1
-  "
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name
+  FROM characters"
 end
