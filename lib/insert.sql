410f1fc0d68e331c5f441fcb76b4a42b35187145
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 16:57:04 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -7,9 +7,11 @@ INSERT INTO subgenres(name) VALUES("SciFi");
 INSERT INTO authors(name) VALUES("Pierce Brown");
 INSERT INTO authors(name) VALUES("Rachel Aaron");
 
-INSERT INTO books(title, year, series_id) VALUES("Red Rising", 2014, 1)
-INSERT INTO books(title, year, series_id) VALUES("Golden Sun", 2015, 1)
-INSERT INTO books(title, year, series_id) VALUES("Morning Star", 2016, 1)
-INSERT INTO books(title, year, series_id) VALUES("Red Rising", 2014, 1)
-INSERT INTO books(title, year, series_id) VALUES("Red Rising", 2014, 1)
-INSERT INTO books(title, year, series_id) VALUES("Red Rising", 2014, 1)
+INSERT INTO books(title, year, series_id) VALUES("Red Rising", 2014, 1);
+INSERT INTO books(title, year, series_id) VALUES("Golden Sun", 2015, 1);
+INSERT INTO books(title, year, series_id) VALUES("Morning Star", 2016, 1);
+INSERT INTO books(title, year, series_id) VALUES("Nice Dragons Finish Last", 2014, 2);
+INSERT INTO books(title, year, series_id) VALUES("One Good Dragon Deserves Another", 2015, 2);
+INSERT INTO books(title, year, series_id) VALUES("No Good Dragon Goes Unpunished", 2016, 2);
+
+INSERT INTO chracters()
