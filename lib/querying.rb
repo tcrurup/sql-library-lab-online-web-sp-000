041fc65de2937e24fce852f310d11f9db2293468
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 17:52:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,7 +32,7 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.species, COUNT(characters.species)
+  "SELECT series.title, characters.species, COUNT(*)
   FROM characters
   INNER JOIN series
     ON characters.series_id = series.id
