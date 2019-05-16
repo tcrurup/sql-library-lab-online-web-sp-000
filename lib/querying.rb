98fdc5b91b7914b80b7cf6fa19124cc88cb07af8
Anthony Rurup <tcrurup@gmail.com>
2019-05-16 17:30:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -18,7 +18,8 @@ def select_value_and_count_of_most_prolific_species
   "SELECT characters.species, COUNT(characters.species)
   FROM characters
   GROUP BY characters.species
-  ORDER BY COUNT (characters.species)"
+  ORDER BY COUNT (characters.species) DESC
+  LIMIT 1"
 end
 
 def select_name_and_series_subgenres_of_authors
